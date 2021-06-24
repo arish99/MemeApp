cc9a4a38f421f4fd93283a1c57bc50c4d9bbf825
arish <arish.ahmad111@gmail.com>
2021-06-24 23:57:46 +0530

    first commit

---
+++ app/src/main/java/com/arish1999/myapplication/MainActivity.java
@@ -0,0 +1,102 @@
+package com.arish1999.myapplication;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+    String url = "https://meme-api.herokuapp.com/gimme";
+    ImageView imageView;
+    ProgressBar progressBar;
+    String imgURl;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageView = findViewById(R.id.imageView);
+        progressBar =  findViewById(R.id.progressBar);
+        getSupportActionBar().hide();
+        getMeme();
+
+    }
+
+    private void getMeme() {
+        progressBar.setVisibility(View.VISIBLE);
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                             imgURl = response.getString("url");
+                            Glide.with(MainActivity.this).load(imgURl).listener(new RequestListener<Drawable>() {
+                                @Override
+                                public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                                    progressBar.setVisibility(View.GONE);
+                                    return false;
+                                }
+
+                                @Override
+                                public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                                    progressBar.setVisibility(View.GONE);
+                                    return false;
+                                }
+                            }).into(imageView);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(MainActivity.this, "Error getting meme", Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+        queue.add(jsonObjectRequest);
+
+
+    }
+
+    public void nextMeme(View view) {
+        getMeme();
+    }
+
+    public void shareMeme(View view) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT,"Check out this meme: "+imgURl);
+        Intent chooser = Intent.createChooser(intent,"Share this meme using...");
+        startActivity(chooser);
+    }
+}
